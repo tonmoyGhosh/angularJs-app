80ef6385f57636472b68e14403358756497e0525
tonmoyanik65@gmail.com <tonmoyanik65@gmail.com>
2021-07-04 14:36:19 +0600

    route configure

--- module/masterApp.js
+++ module/masterApp.js
@@ -1,5 +1,28 @@
-var masterApp = angular.module('masterApp', []);
+var masterApp = angular.module('masterApp', ['ngRoute']);
 
+// Route Configure
+
+masterApp.config(['$routeProvider', function($routeProvider)
+{
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'view/home.html'
+		})
+		.when('/listItem',{
+			templateUrl: 'view/listItem.html',
+			controller: 'ItemListController'
+		})
+		.when('/addItem',{
+			templateUrl: 'view/addItem.html',
+			controller: 'ItemAddController'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		});
+
+}]);
+
+// Controller Configure
 
 masterApp.controller('ItemListController', function($scope)
 {
