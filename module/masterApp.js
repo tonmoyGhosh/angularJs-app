f811aa192fbc01aab6330b37394ea4963ce0b220
tonmoyanik65@gmail.com <tonmoyanik65@gmail.com>
2021-07-04 12:53:38 +0600

    route configure

--- module/masterApp.js
+++ module/masterApp.js
@@ -4,6 +4,15 @@ var masterApp = angular.module('masterApp', []);
 masterApp.controller('ItemListController', function($scope)
 {
 	
-	$scope.message = "Angular";
+	$scope.message = "List Items";
 
 });
+
+
+masterApp.controller('ItemAddController', function($scope)
+{
+	
+	$scope.message = "Add Items";
+
+});
+
